5d8df363c6018fcb3e5c7dbc1c66b1173fa88963
SHINYATANAKA123 <123asde.med@gmail.com>
2021-06-18 17:26:44 +0900

    [Add] カード詳細機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
-  resources :list, only: %i(new create edit update destroy)do
-   resources :card, only: %i(new create)
-end
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, only: %i(new create show)
+  end
 end
